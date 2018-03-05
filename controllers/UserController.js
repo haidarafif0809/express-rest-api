5abb67ca549ea284c4117eb78472b66ff9acbbba
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-03-05 13:27:26 +0700

    Crud REST API User

---
+++ controllers/UserController.js
@@ -0,0 +1,73 @@
+const models = require('../models');
+
+module.exports = {
+  getUsers: (req,res) => {
+    models.User.all().then((users) => {
+      res.status(200).json({
+        message: "Get All User",
+        users
+      });
+    }).catch((err) => {
+      res.status(500).json({
+        message: err.message
+      });
+    });
+  },
+  getUser: (req, res) => {
+    const id = req.params.id;
+    models.User.findById(id).then((user) => {
+      res.status(200).json({
+        message: `Get an User with id ${id}`,
+        user
+      });
+    }).catch((err) => {
+      res.status(500).json({
+        message: err.message
+      });
+    });
+  },
+  createUser: (req, res) => {
+    models.User.create(req.body).then((user) => {
+      res.status(201).json({
+        message: `Success Created New User`,
+        user
+      });
+    }).catch((err) => {
+      res.status(500).json({
+        message: err.message
+      });
+    });
+  },
+  deleteUser: (req, res) => {
+    const id = req.params.id;
+    let userData;
+    models.User.findById(id).then((user) => {
+      userData = user;
+      return user.destroy();
+    }).then((user) => {
+      res.status(200).json({
+        message: `Success Deleted an User`,
+        user: userData
+      });
+    }).catch((err) => {
+      res.status(404).json({
+        message: err.message
+      });
+    });
+  },
+  updateUser: (req, res) => {
+    const id = req.params.id;
+    models.User.findById(id).then((user) => {
+      return user.update(req.body);
+    }).then((user) => {
+      res.status(200).json({
+        message: `Success Updated an User`,
+        user
+      });
+    }).catch((err) => {
+      res.status(404).json({
+        message: err.message
+      });
+    });
+  },
+};
--- routes/users.js
+++ routes/users.js
@@ -1,2 +1,10 @@
 const router = require('express').Router();
+const {getUsers, getUser, createUser, deleteUser, updateUser} = require('../controllers/UserController');
+
+router.get('/', getUsers);
+router.get('/:id', getUser);
+router.post('/', createUser);
+router.delete('/:id', deleteUser);
+router.put('/:id', updateUser);
+
 module.exports = router;
