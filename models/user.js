15bf31c452c5a4a92fb936fefe9b090005c18bc3
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-03-05 13:41:51 +0700

    Add role to user

--- models/user.js
+++ models/user.js
@@ -5,7 +5,8 @@ module.exports = (sequelize, DataTypes) => {
   var User = sequelize.define('User', {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
-    password: DataTypes.STRING
+    password: DataTypes.STRING,
+    role: DataTypes.STRING,
   }, {});
   User.associate = function(models) {
     // associations can be defined here
@@ -13,7 +14,7 @@ module.exports = (sequelize, DataTypes) => {
   User.beforeCreate((user) => {
 
     user.password = bcrypt.hashSync(user.password,10);
-    
+
   })
   return User;
 };
--- seeders/20180305050349-UsersSeeder.js
+++ seeders/20180305050349-UsersSeeder.js
@@ -11,14 +11,26 @@ module.exports = {
       let name = faker.name.findName();
       let email = faker.internet.email();
       let password = bcrypt.hashSync('rahasia',saltRounds);
+      let role = 'member';
       users.push({
         email: email,
         name: name,
         password: password,
+        role: role,
         createdAt: new Date(),
         updatedAt: new Date(),
       });
     }
+
+    let password = bcrypt.hashSync('rahasia',saltRounds);
+    users.push({
+      email: 'admin@gmail.com',
+      name: 'admin',
+      password: password,
+      role: 'admin',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
     return queryInterface.bulkInsert('Users', users, {});
   },
 
